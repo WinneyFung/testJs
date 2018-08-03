4b284860ec3d6758696b5ba426131d2b7fb4efd2
winneyfung <1042214576@qq.com>
2018-08-03 17:47:15 +0800

    取消事件以及事件冒泡

--- js/common.js
+++ js/common.js
@@ -1,3 +1,24 @@
 function my$(id) {
   return document.getElementById(id);
 }
+//绑定事件的兼容
+function addEventListener(element,type,fn) {
+  if(element.addEventListener){
+    element.addEventListener(type,fn,false);
+  }else if(element.attachEvent){
+    element.attachEvent("on"+type,fn);
+  }else{
+    element["on"+type]=fn;
+  }
+}
+//解绑事件的兼容
+//为任意的一个元素,解绑对应的事件
+function removeEventListener(element,type,fnName) {
+  if(element.removeEventListener){
+    element.removeEventListener(type,fnName,false);
+  }else if(element.detachEvent){
+    element.detachEvent("on"+type,fnName);
+  }else{
+    element["on"+type]=null;
+  }
+}
