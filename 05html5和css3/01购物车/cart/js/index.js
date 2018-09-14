954ad127ec4d4334a5b8b21e86413072e920c488
winneyfung <1042214576@qq.com>
2018-09-14 08:02:23 +0800

    daj和html5购物车结构

--- 05html5和css3/01购物车/cart/js/index.js
+++ 05html5和css3/01购物车/cart/js/index.js
@@ -2,5 +2,9 @@ $(() => {
   $(".container").fullpage({
     //配置页面的背景颜色
     sectionsColor: ["#fadd67", "#84a2d4", "#ef674d", "#ffeedd", "#d04759", "#84d9ed", "#8ac060"],
+    //添加的小圆点的导航栏
+    navigation:true,
+    //小圆点导航栏的位置 默认值为right 可选值//"left"
+    navigationPosition:"right"
   });
 });
