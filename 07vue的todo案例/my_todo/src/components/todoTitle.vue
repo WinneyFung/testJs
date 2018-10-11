b382ecbbc2ef1912979d0cad41918ec0d659b97b
winneyfung <1042214576@qq.com>
2018-10-11 11:09:11 +0800

    忽略小程序项目

---
+++ 07vue的todo案例/my_todo/src/components/todoTitle.vue
@@ -0,0 +1,17 @@
+<template>
+    <h3>{{title}}</h3>
+</template>
+<script>
+export default {
+  props: ["title"]
+};
+</script>
+<style>
+h3 {
+  height: 40px;
+  line-height: 40px;
+  font-weight: normal;
+  width: 320px;
+  background-color: #0dacf0;
+}
+</style>
