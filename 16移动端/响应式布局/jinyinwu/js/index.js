e13ad5d22f70904ae682b03f0f6674a14d7a63cb
1042214576@qq.com <1042214576@qq.com>
2019-03-17 16:45:33 +0800

    update：使用bootstrap完成响应式布局

--- 16移动端/响应式布局/jinyinwu/js/index.js
+++ 16移动端/响应式布局/jinyinwu/js/index.js
@@ -1,1 +1,27 @@
+$(function () {
+    initMobileTab();
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+      })
+});
 
+const initMobileTab = function () {
+    const $tabs = $('.product .nav-tabs');
+    let width = 0;
+    /**
+     * width() :获取的是内容的宽度
+     * innerwidth：内边距+内容宽度
+     * outerwidth：边框+内边距+内容宽度
+     * outerwidth（true）：外边距+边框+内边距+内容的宽度
+     */
+    $tabs.find('li').each((i, li) => {
+        $li = $(li);
+        width += $li.outerWidth(true);
+    });
+    $tabs.width(width);
+
+    new IScroll(document.querySelector('.product .tab-parent'), {
+        scrollX: true, scrollY: false,
+        click: true
+    });
+}
